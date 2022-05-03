39361ae0cce4b84103f5bfd39b0b4fd3436374ea
Tom Milner <tom.f.milner@gmail.com>
2022-05-03 11:21:52 +0100

    Updated submission script.

--- CreateSubmission.sh
+++ CreateSubmission.sh
@@ -3,4 +3,4 @@
 zip  Submission Array.jack Keyboard.jack Math.jack Output.jack Screen.jack String.jack \
 Sys.jack Memory.jack compiler.h compiler.c symbols.h symbols.c lexer.c lexer.h parser.h \
 parser.c vm_writer.c vm_writer.h CodeGrader.c ArrayTest* Average* ConvertToBin* Fraction* \
-HelloWorld* List* MathTest* Pong*  Square* Seven* Submission.zip -X -r
+HelloWorld* List* MathTest* Pong*  Square* Seven* CompileAndRun.sh Submission.zip -X -r
